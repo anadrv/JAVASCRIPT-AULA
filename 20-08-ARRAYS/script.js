e593db28a30c2f74da4f4f0ca05d65c715447bb6
Ana Luisa <anadrvv@gmail.com>
2025-08-20 10:36:43 -0300

    aula

---
+++ 20-08-ARRAYS/script.js
@@ -0,0 +1,18 @@
+let xdinaryHeroes = ["Gunil", "Jungsu", "Gaon", "Ode", "Junhan", "Jooyeon", "Jooyeon"];
+
+console.log(xdinaryHeroes);
+
+document.writeln(xdinaryHeroes);
+
+let um = xdinaryHeroes[0];
+let dois = xdinaryHeroes[1];
+
+console.log(um);
+document.writeln(dois);
+
+let maknae = xdinaryHeroes.indexOf("Jooyeon");
+console.log(maknae);
+
+//xdinaryHeroes.push("Ham-il", "Nyangduu", "Jiduck", "Fox.De", "Tyonen", "JJuf");
+xdinaryHeroes.unshift("Ham-il", "Nyangduu", "Jiduck", "Fox.De", "Tyonen", "JJuf");
+console.log(xdinaryHeroes);
