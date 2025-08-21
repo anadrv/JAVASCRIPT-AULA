e47460c6db051de610cee14c2d79047957a65eac
Ana Luisa <anadrvv@gmail.com>
2025-08-21 09:18:52 -0300

    add

---
+++ 20-08-ARRAYS/atividade/script.js
@@ -0,0 +1,9 @@
+let mesa = ["ana"];
+
+
+mesa.push("sabrina", "lavinia");
+mesa.unshift("gabriel", "carol","julia");
+
+mesa.shift();
+
+document.writeln(mesa);
--- 20-08-ARRAYS/script.js
+++ 20-08-ARRAYS/script.js
@@ -15,4 +15,13 @@ console.log(maknae);
 
 //xdinaryHeroes.push("Ham-il", "Nyangduu", "Jiduck", "Fox.De", "Tyonen", "JJuf");
 xdinaryHeroes.unshift("Ham-il", "Nyangduu", "Jiduck", "Fox.De", "Tyonen", "JJuf");
+//xdinaryHeroes.shift(); - remove o primeiro
+//xdinaryHeroes.pop(); - remove o ultimo
+
+
+//xdinaryHeroes.splice(um, 1); - remove pelo indice
+
+const nome = "-"
+
+xdinaryHeroes.splice(1, 0, nome)
 console.log(xdinaryHeroes);
