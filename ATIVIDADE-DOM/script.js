6810028b8b894c14c98851d26f0efb919e4e9334
Ana Luisa <anadrvv@gmail.com>
2025-08-27 11:15:58 -0300

    DOM

--- ATIVIDADE-DOM/script.js
+++ ATIVIDADE-DOM/script.js
@@ -1,13 +1,25 @@
-let azul = document.querySelectorAll(".azul");
-let vermelho = document.querySelectorAll(".vermelho");
 
-azul.forEach(azul => {
+let btn = document.getElementById("btn");
+
+function changeColors() {
+
+    let corAzul = document.querySelectorAll(".azul");
+let corVermelho = document.querySelectorAll(".vermelho");
+
+    corAzul.forEach(azul => {
     azul.classList.add("vermelho");
     azul.classList.remove("azul");
 });
 
-azul.forEach(vermelho => {
+corVermelho.forEach(vermelho => {
     vermelho.classList.add("azul");
     vermelho.classList.remove("vermelho");
 });
+}
+
+btn.addEventListener("click", changeColors);
+
+
+
+
 
