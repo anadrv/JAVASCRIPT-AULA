72fc8eefe2e70077a7b60dae0b094480d2625ff3
Ana Luisa <anadrvv@gmail.com>
2025-08-28 10:09:38 -0300

    form

--- 28-08-FORMS/script.js
+++ 28-08-FORMS/script.js
@@ -4,3 +4,13 @@ const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 const jobSelect = document.querySelector("#job");
 const messageTextarea = document.querySelector("#message");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault(); 
+
+    if (nameInput.value === "") {
+        alert("Por favor, preencha o seu nome");
+        return;
+    }
+    form.submit();
+});
