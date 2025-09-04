fda3855e3b33bb3f03c2e919226e29cfc0aedbeb
Ana Luisa <anadrvv@gmail.com>
2025-09-04 10:06:12 -0300

    modal

--- 04-09-MODAL/script.js
+++ 04-09-MODAL/script.js
@@ -1,9 +1,17 @@
-const button = document.querySelector("button");
+const button = document.querySelector(".btnOpen");
 const modal = document.querySelector("dialog");
+const btn = document.querySelector(".btn")
+
+
 
 button.onclick = function () {
     modal.showModal()
 };
+
+btn.onclick = function () {
+    modal.close()
+};
+
 
 //const button = document.querySelector("button");
 //const modal = document.querySelector("dialog");
