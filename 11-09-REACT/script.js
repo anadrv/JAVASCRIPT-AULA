47910039bf4006bd84645a6d0b0cbb57da92b277
Ana Luisa <anadrvv@gmail.com>
2025-09-17 07:59:34 -0300

    aula

---
+++ 11-09-REACT/script.js
@@ -0,0 +1,9 @@
+ReactDOM.render(
+    "I gotta run away, I wanna run away, but how?", //o que vai renderizar
+    document.getElementById("app") //onde
+)
+
+ReactDOM.render(
+    "man in the box",
+    document.getElementById("music")
+)
