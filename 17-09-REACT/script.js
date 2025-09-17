c6ec5aea3661fee385c696bf208c6cbfd05dd3f2
Ana Luisa <anadrvv@gmail.com>
2025-09-17 10:27:54 -0300

    OK

---
+++ 17-09-REACT/script.js
@@ -0,0 +1,19 @@
+ReactDOM.render(
+    <h1>I gotta run away, I wanna run away, but how?</h1>, 
+    document.getElementById("app") 
+)
+
+ReactDOM.render(
+    <i>as</i>, 
+    document.getElementById("app1") 
+)
+
+ReactDOM.render(
+    <ul>
+        <li>man</li>
+        <li>in</li>
+        <li>the</li>
+        <li>box</li>
+    </ul>,
+    document.getElementById("app3")
+)
